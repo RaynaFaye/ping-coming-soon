9505d8e5f9deb88a5b64f7868fa8a9c1fe557514
Serena <serenafayelombardo@gmail.com>
2020-05-21 16:57:30 +0200

    Functionality for the input email added and extra tooltip

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('.form');
+const emailInput = form.querySelector('.form__input-email');
+const tooltip = document.querySelector('.tooltip');
+const tooltipTwo = document.querySelector('.tooltip-two');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const emailInputValue = event.target.email.value;
+  const regexEmail = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+  if (!regexEmail.test(emailInputValue)) {
+    tooltip.style.visibility = 'visible';
+    tooltip.style.marginBottom = '25px';
+    emailInput.style.border = '1px solid hsl(354, 100%, 66%)';
+  } else {
+    tooltip.style.visibility = 'hidden';
+    tooltip.style.marginBottom = '0px';
+    tooltipTwo.style.display = 'block';
+    emailInput.style.border = '1px solid hsl(0, 0%, 59%)';
+  }
+  setTimeout(function () {
+    tooltipTwo.style.display = 'none';
+  }, 3000);
+  form.reset();
+});
